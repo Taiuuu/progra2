2ca144e806abdd8fe4b946754d892b9d3e9fbba2
Taiu <taielclot@gmail.com>
2026-04-17 12:19:04 -0300

    ejercicio5 completado

--- src/ejercicio5/Main.java
+++ src/ejercicio5/Main.java
@@ -5,27 +5,31 @@ un arreglo con la suma de cada fila de la matriz (cuya dimensión sea la cantida
 matriz). Crear otro que haga lo mismo, pero con las columnas.
  */
 public class Main {
-	//Metodo 1: Suma elementos de cada fila de la matriz
+	//Metodo 1: devuelve un arreglo con la suma de cada fila
 	public static int[] sumaFilas(int[][] mat) {
-		int[] resultado = new int[mat.length];
-			
+	    int[] vector = new int[mat.length];
+		
 		for(int f = 0; f<mat.length; f++) {
+			int sumatoria = 0;
 			for(int c = 0; c<mat[f].length; c++) {
 				sumatoria += mat[f][c];
 			}
+			vector[f] = sumatoria;
 		}		
-		return sumatoria;
+		return vector;
 	}
-	//Metodo 2: Suma elementos de la columna de la matriz
+	//Metodo 2: devuelve un arreglo con la suma de cada columna
 		public static int[] sumaColumnas(int[][] mat) {
-			int[] resultado = new int[mat[0].length];
-				
-			for(int f = 0; f<mat.length; f++) {
-				for(int c = 0; c<mat[f].length; c++) {
+			int[] vector = new int[mat[0].length];
+			
+			for(int c = 0; c<mat[0].length; c++) {
+				int sumatoria = 0;
+				for(int f = 0; f<mat.length; f++) {
 					sumatoria += mat[f][c];
 				}
+				vector[c] = sumatoria;
 			}		
-			return sumatoria;
+			return vector;
 		}
 		
 }
